0f773832005f833d0c479173a06a5e3b66f7dce8
akmal-2004 <akmallatifov2004@gmail.com>
2024-01-23 16:13:58 +0500

    added default admin user

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import uvicorn
+from models import User
+from databases import users
+
+user = User(first_name='Admin', last_name='Admin', email='admin@gmail.com', balance=1000000, username='admin', password='admin')
+users.create_user(user.dict())
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
